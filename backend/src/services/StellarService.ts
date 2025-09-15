a837f9bd84651c2fd13fe2a37d691433350ea002
josiasdev <francisco.batista67@alu.ufc.br>
2025-09-15 17:12:24 -0300

    feat: chamando os tipos

--- backend/src/services/StellarService.ts
+++ backend/src/services/StellarService.ts
@@ -1,27 +1,7 @@
-import { Keypair } from "@stellar/stellar-sdk";
-
+import type { StellarConfig, PaymentData, ContractData } from "./stellar.types";
 const StellarSdk = require('@stellar/stellar-sdk');
 
-export interface StellarConfig {
-  horizonUrl: string;
-  network: string;
-  passphrase: string;
-}
 
-export interface PaymentData {
-  fromKeypair: Keypair;
-  toPublicKey: string;
-  amount: string;
-  memo?: string;
-}
-
-export interface ContractData {
-  merchantPublicKey: string;
-  customerPublicKey: string;
-  totalAmount: string;
-  installmentsCount: number;
-  terms: Record<string, any>;
-}
 
 export class StellarService {
   private server: any;
